f7fc427fefd51344cade60afc9edfd35614b629e
Praveen Kumar <praveenscience@gmail.com>
2022-09-25 12:48:05 +0100

    WIP

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
+import Header from "./Header/Header";
 
 export default class App extends Component {
+  state = {
+    Dark: false
+  };
   render() {
-    return <div className="App">Hello Tech Table!</div>;
+    return (
+      <div className="App">
+        <Header dark={this.state.Dark} className="Header">
+          Tech Table
+        </Header>
+        <div className="container">
+          <div className="row">
+            <div className="col-6 offset-6">
+              <label>
+                <input
+                  type="checkbox"
+                  checked={this.state.Dark}
+                  onChange={e => this.setState({ Dark: e.target.checked })}
+                />
+                Toggle
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
